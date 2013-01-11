a4b4a9207076f04f22c8e49430dcd2ad97173db7
unknown <andre@.(none)>
2013-01-10 22:33:24 -0200

    add

---
+++ module/Application/src/Application/Form/AreaStep3Form.php
@@ -0,0 +1,49 @@
+<?php
+namespace Application\Form;
+
+use Zend\Form\Form;
+//use Application\Form\FaleConoscoFilter;
+
+class AreaStep3Form extends Form
+{
+    public function __construct($baseUrl = null)
+    {
+        // we want to ignore the name passed
+        parent::__construct('step3');
+
+        $this->setAttribute('method', 'post');
+        
+        $this->add(array(
+            'name' => 'titulo',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' =>'campotxt'
+            ),
+            'options' => array(
+                'label' => 'Titulo',
+            ),
+        ));
+        
+
+        $this->add(array(
+            'name' => 'url',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' =>'campotxt'
+            ),
+            'options' => array(
+                'label' => 'URL',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Enviar',
+                'id' => 'submitbutton',
+            ),
+        ));
+
+    }
+}
