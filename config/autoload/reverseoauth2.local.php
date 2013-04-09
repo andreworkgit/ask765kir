1ab9a975b0b8fe5df7d3516188eba4674e512ff0
Andre Augusto <andrework@gmail.com>
2013-04-09 16:35:04 -0300

    test

---
+++ config/autoload/reverseoauth2.local.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+    'reverseoauth2' => array(
+        'google' => array(
+            'client_id'     => '740383275630.apps.googleusercontent.com',
+            'client_secret' => 'NFUXXX1dBbN6Dle-8T8DWTRQ',
+            'redirect_uri'  => 'http://127.0.0.10/faleconosco',
+        ),
+        'github' => array(
+            'client_id'     => 'your id',
+            'client_secret' => 'your secret',
+            'redirect_uri'  => 'your callback url which links to your controller',
+        ),
+        'facebook' => array(
+            'client_id'     => 'your id',
+            'client_secret' => 'your secret',
+            'redirect_uri'  => 'your callback url which links to your controller',
+        ),
+        'linkedin' => array(
+            'client_id'     => 'your api key',
+            'client_secret' => 'your api secret',
+            'redirect_uri'  => 'your callback url which links to your controller',
+        ),
+    )
+);
